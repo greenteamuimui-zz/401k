3a25d2ad3f818872dff23d8018f653b32c64f2bc
greenteamuimui <siuding0601@gmail.com>
2017-10-10 18:07:19 -0700

    Test portfolio class

---
+++ spec/portfolio_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'csv'
+require 'portfolio'
+
+describe Portfolio do
+  let(:risk_symbol_location) do
+    "data/risk_symbol.csv"
+  end
+  let(:symbol_price_location) do
+    "data/symbol_price.csv"
+  end
+  subject(:portfolio) {Portfolio.new(risk_symbol_location,symbol_price_location)}
+
+  describe '#initialize' do
+    it "made hash with the risk symbol array and stores it" do
+      expect(portfolio.risk_hash).not_to be_empty
+    end
+
+    it "made hash with the symbol price hash and stores it" do
+      expect(portfolio.symbol_price_hash).not_to be_empty
+    end
+  end
+
+  describe '#employee_portfolio' do
+    it "calculate the portfolio using risk and total and returns a array of arrays with each share's info" do
+      expect(portfolio.employee_portfolio(1, 1000)).to eq([["VFIAX", "300.00", "30", "28.79"], ["VTSAX", "300.00", "30", "14.84"], ["VGSLX", "400.00", "40", "336.13"]])
+    end
+  end
+
+end
